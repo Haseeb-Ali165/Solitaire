ebd2c8043722223c39f44c95e51a9cbb2282f1f9
Haseeb-Ali165 <149551635+Haseeb-Ali165@users.noreply.github.com>
2024-10-08 10:21:32 +0500

    Create Solitaire.cpp

---
+++ Solitaire.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <vector>
+#include "Board.h"
+#include <SFML/Graphics.hpp>
+using namespace std;
+
+int main()
+{
+
+    Board b;
+    b.initBoard();
+  
+
+    return 0;
+}
