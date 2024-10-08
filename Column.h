09af4476dc6846b9a12b6d10db89c0fe1a973b0a
Haseeb-Ali165 <149551635+Haseeb-Ali165@users.noreply.github.com>
2024-10-08 18:58:32 +0500

    Update Column.h

--- Column.h
+++ Column.h
@@ -24,6 +24,37 @@ private:
 public:
     sf::FloatRect bounds;
 
+    Stock() {
+        bounds.left = 30;
+        bounds.left = 150;
+        bounds.width = 130;
+        bounds.height = 170;
+    }
+
+    void addCard(Card& card) {
+        pile.push_back(card);
+    }
+
+    // Function to return the current pile (for drawing/rendering)
+    vector<Card>& getPile() {
+        return pile;
+    }
+};
+
+class Waste {
+private:
+    vector<Card> pile;
+
+public:
+    sf::FloatRect bounds;
+
+    Waste() {
+        bounds.left = 30;
+        bounds.top = 340;
+        bounds.width = 130;
+        bounds.height = 170;
+    }
+
     void addCard(Card& card) {
         pile.push_back(card);
     }
